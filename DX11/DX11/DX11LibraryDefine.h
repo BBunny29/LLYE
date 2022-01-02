09dbc54d7bc49c678d275af2b35b4b26b0f8cb71
BBunny29 <burningbunny29@gmail.com>
2022-01-03 00:55:51 +0900

    Change DX11 Define

--- DX11/DX11/DX11LibraryDefine.h
+++ DX11/DX11/DX11LibraryDefine.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#pragma comment(lib, "d3d11")
+#pragma comment(lib, "DXGI")
+#pragma comment(lib, "D3DCompiler")
+
 // Engine
 #ifdef _WIN64
 #ifdef _DEBUG
@@ -14,10 +18,5 @@
 #pragma comment(lib, "JJEngine")
 #endif
 #endif
-//
-//#pragma comment(lib, "d3d11")
-//#pragma comment(lib, "DXGI")
-//#pragma comment(lib, "D3DCompiler")
-//d3d11.lib
-//DXGI.lib
-//D3DCompiler.lib
+
+
