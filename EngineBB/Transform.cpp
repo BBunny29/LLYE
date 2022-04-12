6a8778a7e414bac071407edeb9e557ace0a697f8
BBunny29 <burningbunny29@gmail.com>
2022-04-12 21:50:24 +0900

    transform

--- EngineBB/Transform.cpp
+++ EngineBB/Transform.cpp
@@ -2,9 +2,43 @@
 #include "Transform.h"
 
 Transform::Transform()
+	: m_pos(DirectX::SimpleMath::Vector3())
+	, m_rot(DirectX::SimpleMath::Vector3())
+	//, m_rotQ(DirectX::SimpleMath::Quaternion())
+	, m_scale(DirectX::SimpleMath::Vector3())
+	, m_posXMVec(DirectX::XMVECTOR())
+	, m_rotXMVec(DirectX::XMVECTOR())
+	, m_rotQXMVec(DirectX::XMVECTOR())
 {
 }
 
 Transform::~Transform()
 {
+}
+
+const DirectX::SimpleMath::Vector3& Transform::GetPositionFloat3() const
+{
+	return this->m_pos;
+}
+
+const DirectX::XMVECTOR& Transform::GetPositionVector() const
+{
+	return this->m_posXMVec;
+}
+
+const DirectX::SimpleMath::Vector3& Transform::GetRotationFloat3() const
+{
+	return this->m_rot;
+}
+
+const DirectX::XMVECTOR& Transform::GetRotationVector() const
+{
+	return this->m_rotXMVec;
+}
+
+void Transform::SetPosition(const DirectX::SimpleMath::Vector3& pos)
+{
+	m_pos = pos;
+	m_posXMVec = m_pos;
+	//UpdateMatrix();
 }
--- EngineBB/Transform.h
+++ EngineBB/Transform.h
@@ -3,11 +3,41 @@
 
 class Transform
 {
+public:
 	Transform();
 	~Transform();
 
-	DirectX::SimpleMath::Vector3 m_pos;
-	DirectX::SimpleMath::Vector3 m_rot;
-	DirectX::SimpleMath::Quaternion m_scale;
+public:
+	const DirectX::SimpleMath::Vector3& GetPositionFloat3() const;
+	const DirectX::XMVECTOR& GetPositionVector() const;
+	const DirectX::SimpleMath::Vector3& GetRotationFloat3() const;
+	const DirectX::XMVECTOR& GetRotationVector() const;
+
+	void SetPosition(const DirectX::SimpleMath::Vector3& pos);
+	void SetPosition(const DirectX::XMVECTOR& pos);
+	void SetPosition(float x, float y, float z);
+	void AdjustPosition(const DirectX::SimpleMath::Vector3& pos);
+	void AdjustPosition(const DirectX::XMVECTOR& pos);
+	void AdjustPosition(float x, float y, float z);
+
+	void SetRotation(const DirectX::SimpleMath::Vector3& rot);
+	//void SetRotation(const DirectX::SimpleMath::Quaternion& rotQ);
+	void SetRotation(const DirectX::XMVECTOR& rot);
+	void SetRotation(float x, float y, float z);
+	void AdjustRotation(const DirectX::SimpleMath::Vector3& rot);
+	void AdjustRotation(const DirectX::XMVECTOR& rot);
+	void AdjustRotation(float x, float y, float z);
+	
+	void SetScale(float xScale, float yScale, float zScale = 1.0f);
+
+private:
+	DirectX::SimpleMath::Vector3	m_pos;
+	DirectX::SimpleMath::Vector3	m_rot;
+	//DirectX::SimpleMath::Quaternion m_rotQ;
+	DirectX::SimpleMath::Vector3	m_scale;
+
+	DirectX::XMVECTOR m_posXMVec;
+	DirectX::XMVECTOR m_rotXMVec;
+	DirectX::XMVECTOR m_rotQXMVec;
 };
 
