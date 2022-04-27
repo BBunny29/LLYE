31eba7f67e635fec2e09561b6f93a191dbd298f8
BBunny29 <burningbunny29@gmail.com>
2022-04-27 21:08:01 +0900

    Analyze variable arguments

--- Assistance/DebugString.h
+++ Assistance/DebugString.h
@@ -18,6 +18,36 @@ namespace Debug
 	inline void PrintDebugString(char const* const format, ...)
 	{
 #ifdef _DEBUG
+		// 가변인수들을 저장하는 스택 주소 포인터 
+		va_list args;
+		int     len;
+		char* buffer;
+
+		// args가 첫번째 가변인수를 가리킬 수 있도록 초기화
+		va_start(args, format);
+
+		len = _vscprintf(format, args) + 1; // _vscprintf는 널문자를 doesn't count '\0'. terminating '\0'
+
+		buffer = (char*)malloc(len * sizeof(char));
+
+		if (0 != buffer)
+		{
+			vsprintf_s(buffer, len, format, args);
+			puts(buffer);
+
+			OutputDebugStringA(buffer);
+			OutputDebugStringA("\n");
+
+			free(buffer);
+		}
+		va_end(args);
+#endif
+	}
+
+	template<typename T>
+	inline void PrintDebugStringState(char const* const format, ...)
+	{
+#ifdef _DEBUG
 		va_list args;
 		int     len;
 		char* buffer;
--- OwnLibs/Incs/EngineBB/IEngineBB.h
+++ OwnLibs/Incs/EngineBB/IEngineBB.h
@@ -1,14 +1,11 @@
 #pragma once
 #include <memory>
-__interface IRenderer;
 
+__interface IRenderer;
 
 __interface IEngineBB
 {
 public:
-	virtual HRESULT Init() abstract;
-	virtual int testFunc(int i) abstract;
+	virtual HRESULT Init(int hWND, int width, int height) abstract;
 
-	virtual std::shared_ptr<IRenderer> GetRenderer() abstract;
-	//virtual std::shared_ptr<IRenderer> CreateRenderer(const wchar_t* dllPath) abstract;
 };
