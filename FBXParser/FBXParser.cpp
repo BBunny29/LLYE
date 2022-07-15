9d85842c5ee36f24a0f09724724ebd74144c66a6
BBunny29 <burningbunny29@gmail.com>
2022-07-15 17:51:02 +0900

    making a parser
    파서 제작중

--- FBXParser/FBXParser.cpp
+++ FBXParser/FBXParser.cpp
@@ -14,6 +14,14 @@
 //fbxsdk::FbxMesh* m_pFBXmesh;							// FBX의 매쉬
 //fbxsdk::FbxString m_fbxFileName;						// 파일이름
 
+FBXParser::FBXParser()
+{
+}
+
+FBXParser::~FBXParser()
+{
+}
+
 void FBXParser::Initalize()
 {
 	// FBX SDK Manager 생성
@@ -22,7 +30,7 @@ void FBXParser::Initalize()
 	// Scene 생성
 	m_pScene = FbxScene::Create(m_pManager, "My Scene");
 
-	// IOSettings 객체 생성 및 설정
+	// IOSettings 객체 생성 및 설정(importer나 exporter용으로 설정해줄수 있다.)
 	FbxIOSettings* ios = FbxIOSettings::Create(m_pManager, IOSROOT);
 	m_pManager->SetIOSettings(ios);
 
@@ -42,7 +50,19 @@ void FBXParser::Destroy()
 
 void FBXParser::LoadScene(std::string& fileName, std::string& writePath)
 {
+	const char* _fileName = fileName.c_str();
 
+	std::string _nowWritePath(writePath);
+	_nowWritePath += StringHelper::GetFileName(fileName, false);
+
+	m_fbxFileName = _fileName;
+	m_HasSkeleton = false;
+
+	// 로드가 불릴 때 마다 초기화 해준다
+	m_pFBXModel = std::make_shared<FBXModel>();
+	m_pFBXModel->fileName = fileName;
+
+	SceneSetting();
 }
 
 void FBXParser::SceneSetting()
@@ -50,6 +70,8 @@ void FBXParser::SceneSetting()
 	// 씬 임포트
 	// 여러개의 FBX를 한꺼번에 로드할때 문제가 발생한다.
 	// m_pImporter를 리셋한다음에 다시 만들어야할까?
+	
+	// 하나의 Fbx파일을 로드할때마다(LoadScene) 임포터를 초기화하기로 했다.(2022.07.15)
 	bool status = m_pImporter->Initialize(m_fbxFileName, -1, m_pManager->GetIOSettings());
 	m_pImporter->Import(m_pScene);
 
@@ -61,7 +83,8 @@ void FBXParser::SceneSetting()
 		exit(-1);
 	}
 
-	FbxAxisSystem sceneAxisSystem = m_pScene->GetGlobalSettings().GetAxisSystem(); // 씬 내의 좌표축을 바꾼다. 
+	// 씬 내의 좌표축을 바꾼다. 
+	FbxAxisSystem sceneAxisSystem = m_pScene->GetGlobalSettings().GetAxisSystem(); 
 	FbxAxisSystem::MayaYUp.ConvertScene(m_pScene);
 
 	// 씬 내에서 삼각형화 할 수 있는 모든 노드를 삼각형화 시킨다. 
--- FBXParser/FBXParser.h
+++ FBXParser/FBXParser.h
@@ -1,6 +1,36 @@
 #pragma once
 #include <fbxsdk.h>
 #include <memory>
+#include <vector>
+
+// 파서 바깥으로 내보내지는 구조체
+struct FBXModel
+{
+	FBXModel();
+	~FBXModel();
+
+	std::string fileName;
+	std::vector<std::string> materialName_V;
+	std::vector<std::shared_ptr<JMParserData::Mesh>> pMesh_V;
+
+	// Parser에서 Temp로 만들어진 Skeleton을 깔끔하게 넘겨주기 위해 unique_ptr을 썼다
+	// unique_ptr의 배열형태, 생성 시에도 전혀 다른 방법으로 생성해야 한다
+
+	std::unique_ptr<JMParserData::Skeleton> pSkeleton;
+	std::vector<std::string> animationName_V;
+
+	std::shared_ptr<JMParserData::Mesh> GetMesh(const std::string& name)
+	{
+		for (auto& _nowMesh : pMesh_V)
+		{
+			if (name == _nowMesh->nodeName)
+			{
+				return _nowMesh;
+			}
+		}
+		return nullptr;
+	}
+};
 
 class FBXParser
 {
@@ -17,16 +47,17 @@ private:
 
 private:
 	fbxsdk::FbxManager* m_pManager;							// Fbx sdk의 구성요소를 생성, 관리하는 매니저
-	fbxsdk::FbxScene* m_pScene;								// 3ㅇ
-	fbxsdk::FbxImporter* m_pImporter;						// 
-	fbxsdk::FbxGeometryConverter* m_pGeometryConverter;
-	fbxsdk::FbxMesh* m_pFBXmesh;							// FBX의 매쉬
-	fbxsdk::FbxString m_fbxFileName;						// 파일이름
+	fbxsdk::FbxScene* m_pScene;								// 3D장면에 대한 정보를 가지고 있음(루트노드, 텍스쳐 등등)
+	fbxsdk::FbxImporter* m_pImporter;						// Fbx 파일을 읽어들이는 객체
+	fbxsdk::FbxGeometryConverter* m_pGeometryConverter;		// 
 
-	//std::shared_ptr<FBXModel> m_pFBXModel;					// 모델하나 단위
-	//std::shared_ptr<JMParserData::Mesh> m_pMesh;			// 파서의 매쉬
-	//
-	//std::unique_ptr<JMParserData::Skeleton> m_pSkeleton;
+	fbxsdk::FbxString m_fbxFileName;						// 파일이름
+	fbxsdk::FbxMesh* m_pFBXmesh;							// FBX의 매쉬
+
+	std::shared_ptr<FBXModel> m_pFBXModel;					// 모델하나 단위
+	
+	std::shared_ptr<JMParserData::Mesh> m_pMesh;			// 파서의 매쉬
+	std::unique_ptr<JMParserData::Skeleton> m_pSkeleton;
 	//std::vector<DirectX::SimpleMath::Matrix> m_BoneOffsets;
 
 	bool m_HasSkeleton;
